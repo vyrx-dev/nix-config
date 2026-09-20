cf74c5c5d4bb9b5ae850bcd1370cfa632e5e55bf
vyrx <theamit.969@gmail.com>
2026-09-20 13:26:03 +0530

    add treesitter for nvim compatibility

--- configuration.nix
+++ configuration.nix
@@ -375,6 +375,7 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    tree-sitter
     # ── Wayland / Desktop ───────────────────────────────────────────────
     swaybg
     swaylock-effects
